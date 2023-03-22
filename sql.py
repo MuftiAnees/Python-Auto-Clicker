6f21d2b4aba48eadeef6289b0eece05964f01b2a
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2023-03-22 11:26:40 +0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,24 +1,27 @@
 import subprocess
 import pyautogui
+from time import sleep
 
-# subprocess.Popen('C:\Program Files\Google\Chrome\Application\chrome.exe')
-
-# Returns two integers, the width and height of the screen. (The primary monitor, in multi-monitor setups.)
+subprocess.Popen(
+    'C:\Program Files\MySQL\MySQL Workbench 8.0 CE\MySQLWorkbench.exe')
+sleep(30)
 screenWidth, screenHeight = pyautogui.size()
-# Returns two integers, the x and y of the mouse cursor's current position.
 currentMouseX, currentMouseY = pyautogui.position()
-pyautogui.moveTo(240, 525)  # Move the mouse to the x, y coordinates 100, 150.
-pyautogui.click()  # Click the mouse at its current location.
-pyautogui.click(240, 525)  # Click the mouse at the x, y coordinates 200, 220.
-# Move mouse 10 pixels down, that is, move the mouse relative to its current position.
-pyautogui.move(None, 10)
-pyautogui.doubleClick()  # Double click the mouse at the
-# Use tweening/easing function to move mouse over 2 seconds.
-pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
-# Type with quarter-second pause in between each key.
-pyautogui.write('Hello world!', interval=0.25)
-pyautogui.press('esc')  # Simulate pressing the Escape key.
-pyautogui.keyDown('shift')
-pyautogui.write(['left', 'left', 'left', 'left', 'left', 'left'])
-pyautogui.keyUp('shift')
-pyautogui.hotkey('ctrl', 'c')
+pyautogui.click(240, 525)
+sleep(30)
+pyautogui.click(293, 147)
+pyautogui.click(1917, 3)
+
+# # Move mouse 10 pixels down, that is, move the mouse relative to its current position.
+# pyautogui.move(None, 10)
+# pyautogui.doubleClick()  # Double click the mouse at the
+# # Use tweening/easing function to move mouse over 2 seconds.
+# pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
+# # Type with quarter-second pause in between each key.
+# pyautogui.write('SET GLOBAL sql_mode ="";')
+# pyautogui.write('SET SESSION sql_mode ="";', interval=0.25)
+# pyautogui.press('esc')  # Simulate pressing the Escape key.
+# pyautogui.keyDown('shift')
+# pyautogui.write(['left', 'left', 'left', 'left', 'left', 'left'])
+# pyautogui.keyUp('shift')
+# pyautogui.hotkey('ctrl', 'c')
