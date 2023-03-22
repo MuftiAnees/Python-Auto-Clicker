936dbb94e64a01b180646e58e47a3013065731f3
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2023-03-22 11:47:27 +0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -12,17 +12,3 @@ sleep(25)
 pyautogui.click(293, 147)
 sleep(5)
 pyautogui.click(1917, 3)
-
-# # Move mouse 10 pixels down, that is, move the mouse relative to its current position.
-# pyautogui.move(None, 10)
-# pyautogui.doubleClick()  # Double click the mouse at the
-# # Use tweening/easing function to move mouse over 2 seconds.
-# pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
-# # Type with quarter-second pause in between each key.
-# pyautogui.write('SET GLOBAL sql_mode ="";')
-# pyautogui.write('SET SESSION sql_mode ="";', interval=0.25)
-# pyautogui.press('esc')  # Simulate pressing the Escape key.
-# pyautogui.keyDown('shift')
-# pyautogui.write(['left', 'left', 'left', 'left', 'left', 'left'])
-# pyautogui.keyUp('shift')
-# pyautogui.hotkey('ctrl', 'c')
